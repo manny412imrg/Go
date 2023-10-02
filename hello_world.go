a00eb64cbdd68e16eb66188600854866e7a71a1c
Ivan Romero Gutierrez <manny.parte.2@gmail.com>
2023-10-02 14:32:06 -0400

    algunos datos, variables, formatos

--- hello_world.go
+++ hello_world.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
+
+	//Hola mundo
+	/* Hola mundo
+	en varias lineas*/
 	fmt.Println("Hola, Go")
+	//variables
+	// var "name" "type"
+	var myString string = "Esto es una cadena de texto"
+	fmt.Println(myString)
+	myString = "aqui cambia el valor de la cadena de texto"
+	fmt.Println(myString)
+	var myString2 = "Hola esto es string2 sin tipado"
+	fmt.Println(myString2)
+
 }
